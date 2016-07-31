e8ba7dac25847891cd865dc141d2873de3fe1ed1
alevashov <alevashov@users.noreply.github.com>
2016-07-31 12:10:12 +1000

    Fixes in Combined calculation

--- Data/pedestrian_transport_alex-2.R
+++ Data/pedestrian_transport_alex-2.R
@@ -16,20 +16,32 @@ TramDailyAverage <- 500000
 
 Pedestrians <- filter(Pedestrians,Sensor_Name!='Flinders Street Station Underpass')
 
-l <- nrow(Pedestrians)
+
 Combined <- mutate(Pedestrians, Bus=0, Tram=0, Train=0, PedBusRatio=0, PedTramRatio=0, PedTrainRatio=0)
+l <- nrow(Combined)
 
 for (i in 1:l)
         {
-        y<-year(Pedestrians$Date[i])
+        y<-year(Combined$Date[i])
+        sens <-Combined$Sensor_Name[i]
+        # sub-setting current year data only
+        subs <- filter(Combined,year(Date)==y & Sensor_Name==sens)
         
-        Combined$Tram[i] <- TramDailyAverage
+        Combined$PedestrianAnnualSum[i] <- sum(subs$sum.Hourly_Counts.)
+        
+        
+        Combined$DailyShare[i] <- Combined$sum.Hourly_Counts.[i]/Combined$PedestrianAnnualSum[i]
+        
+        Combined$Tram[i] <- TramDailyAverage * 365 * Combined$DailyShare[i]
        
         Tbus <- filter(Buses, Year==y)
-        Ttrain <- filter(Trains, Year==y)
-        Combined$Bus[i] <- Tbus$Bus.daily   
-        Combined$Train[i] <- Ttrain$Train.daily  
+        Ttrain <- filter(Trains, Year==y)  
+        Combined$TrainAnnualSum[i] <- as.numeric(Ttrain$Train.annual)
+        Combined$Bus[i] <- Tbus$Bus.annual * Combined$DailyShare[i] 
+        Combined$Train[i] <- Combined$TrainAnnualSum[i]  * Combined$DailyShare[i]
+        
         }
+
 
 Combined <- mutate(Combined, PedBusRatio=sum.Hourly_Counts./Bus, PedTramRatio=sum.Hourly_Counts./Tram, PedTrainRatio=sum.Hourly_Counts./Train)
 write.csv(Combined, file="PedTramBusTrainCombined.csv")
