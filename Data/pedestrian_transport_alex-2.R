58f99d42bace44844d9b40c363941d529fd4beb7
alevashov <alevashov@users.noreply.github.com>
2016-07-31 11:07:44 +1000

    Pedestrian data combined with trains, tram and bus

--- Data/pedestrian_transport_alex-2.R
+++ Data/pedestrian_transport_alex-2.R
@@ -5,18 +5,19 @@ library(lubridate)
 
 Pedestrians <- read.csv("Data/Pedestrian/pedestrians by date.csv")
 Buses <- read.csv("Data/Bus-calculated-Alex-data-only.csv",col.names=c("Year", "Bus.annual", "Bus.daily","DataType"))
+Trains <- read.csv("Data/Train/Train-calculated-AL-data-only.csv",col.names=c("Year", "Train.annual", "Train.daily","DataType"))
+
 Pedestrians$Date <- as.Date(Pedestrians$Date)
 
 
 TramDailyAverage <- 500000
 
-Flagstaff = filter(Pedestrians,Sensor_Name=='Flagstaff Station')
-Flinders = filter(Pedestrians,Sensor_Name=='Flinders Street Station Underpass')
-SouthernCross= filter(Pedestrians,Sensor_Name=='Southern Cross Station')
+#Excluding Flinders
 
+Pedestrians <- filter(Pedestrians,Sensor_Name!='Flinders Street Station Underpass')
 
 l <- nrow(Pedestrians)
-Combined <- mutate(Pedestrians, Bus=0, Tram=0, PedBusRatio=0, PedTramRatio=0)
+Combined <- mutate(Pedestrians, Bus=0, Tram=0, Train=0, PedBusRatio=0, PedTramRatio=0, PedTrainRatio=0)
 
 for (i in 1:l)
         {
@@ -25,8 +26,10 @@ for (i in 1:l)
         Combined$Tram[i] <- TramDailyAverage
        
         Tbus <- filter(Buses, Year==y)
+        Ttrain <- filter(Trains, Year==y)
         Combined$Bus[i] <- Tbus$Bus.daily   
+        Combined$Train[i] <- Ttrain$Train.daily  
         }
 
-Combined <- mutate(Combined, PedBusRatio=sum.Hourly_Counts./Bus, PedTramRatio=sum.Hourly_Counts./Tram)
-write.csv(Combined, file="PedTramBusCombined.csv")
+Combined <- mutate(Combined, PedBusRatio=sum.Hourly_Counts./Bus, PedTramRatio=sum.Hourly_Counts./Tram, PedTrainRatio=sum.Hourly_Counts./Train)
+write.csv(Combined, file="PedTramBusTrainCombined.csv")
